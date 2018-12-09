ed94cd08c7e72b8081211ec9141e69ecfec4d6de
渡辺大智 <watanabetaichi@watanabetaichinoMacBook-ea.local>
2018-12-09 17:45:30 +0900

    aaa

--- miyake/christmas_line/render.1.py
+++ miyake/christmas_line/render.1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env_python3
 
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+import split_each_weeks_by_txt_3 as split_each_weeks_by_txt_3
 
 app = Flask(__name__)
 
@@ -14,9 +15,15 @@ def index():
 
 @app.route('/result', methods=['GET', 'POST'])
 def result():
+    key_word = request.form['txt_file']
+    e_personality, number_of_weekss = split_each_weeks_by_txt_3.main(key_word)
+    values_a = []
     if request.method == 'POST':
-        labels = [1, 2, 3, 4, 5, 6]
-        values_a = [10, 50 , 100, 35, 86, 72]
+        for e_p in e_personality:
+            e_p = e_p * 100
+            values_a.append(e_p)
+        labels = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        #values_a = [10, 50 , 100, 35, 86, 72]
         return render_template('result.html', labels=labels, values_a=values_a)
     else:
         return redirect(url_for('index'))
