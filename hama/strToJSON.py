61ea00417f6432c5371d9a01ae3c538b0c027e17
濱千代拓也 <>
2018-12-09 09:53:05 +0900

    line履歴をJSONファイル出力する関数

--- hama/strToJSON.py
+++ hama/strToJSON.py
@@ -1,46 +1,26 @@
 import collections as cl
 import json
 import pprint
+from os import mkdir, remove
+from os.path import join, dirname, abspath, exists
+
+
+
+#line_history.json
+
+def getFileName():
+    json_folder = join(dirname(abspath('__file__')), 'json_folder/')
+    if not exists(json_folder):
+        mkdir(json_folder)
+
+    return join(json_folder,'line_history.json')
 
 def strToJson(str):
-    textArray = str.split('\n')
-    # jsonBof = '{"text":'
-    # jsonComma = ', "text":'
-    # jsonEof = '}'
+    lineHistoryArray = str.split('\n')
+    lineHistoryDict = dict(text = lineHistoryArray)
 
-
-    jsonFile = None
-
-    # count = 1
-    # for text in textArray
-    #     jsonFile += text
-    #
-    #     if len(textArray) > count
-    #         jsonFile += jsonComma
-    #     else:
-    #         jsonFile += jsonEof
-    #
-    #     count += 1
-
-    # for i in range(len(textArray)):
-    #     jsonFile += textArray[i]
-    #
-    #     if len(textArray) > i+1:
-    #         jsonFile += jsonComma
-    #     else:
-    #         jsonFile += jsonEof
-    #
-    # print(json.dumps(jsonFile))
-
-    jsonFile = dict(text = textArray)
-
-    print(json.dumps(jsonFile))
-
-    return json.dumps(jsonFile)
+    with open(getFileName(),'w') as fw:
+        json.dump(lineHistoryDict, fw, indent=2)
 
 if __name__ == '__main__':
-    json = strToJson('sakufhdsfu\noisjdoisj\niusdhius')
-
-
-    # dic = json.load(json)
-    # pprint.pprint(dic, width=40)
+    strToJson('sakufhdsfu\noisjdoisj\niusdhius')
